c6f350e9c533c1de2c39910b358a2ce70f40ca70
ecruzq1 <59055637+ecruzq1@users.noreply.github.com>
2019-12-19 09:03:22 -0500

    Deber Ciudades

---
+++ Form1.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label33 = New System.Windows.Forms.Label()
+        Me.Label44 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Seleccionar:", "Sierra", "Costa", "Oriente"})
+        Me.ComboBox1.Location = New System.Drawing.Point(205, 53)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(149, 24)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "Seleccionar:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(205, 137)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox2.TabIndex = 1
+        Me.ComboBox2.Visible = False
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(352, 137)
+        Me.ComboBox3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox3.TabIndex = 2
+        Me.ComboBox3.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(103, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Region"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(103, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Provincias"
+        '
+        'Label33
+        '
+        Me.Label33.AutoSize = True
+        Me.Label33.Location = New System.Drawing.Point(103, 229)
+        Me.Label33.Name = "Label33"
+        Me.Label33.Size = New System.Drawing.Size(109, 17)
+        Me.Label33.TabIndex = 6
+        Me.Label33.Text = "Provincia Mayor"
+        '
+        'Label44
+        '
+        Me.Label44.AutoSize = True
+        Me.Label44.Location = New System.Drawing.Point(103, 278)
+        Me.Label44.Name = "Label44"
+        Me.Label44.Size = New System.Drawing.Size(110, 17)
+        Me.Label44.TabIndex = 7
+        Me.Label44.Text = "Provincia Menor"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(231, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 17)
+        Me.Label3.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(231, 278)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 17)
+        Me.Label4.TabIndex = 9
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(700, 53)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox1.TabIndex = 10
+        Me.TextBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(700, 103)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox2.TabIndex = 11
+        Me.TextBox2.Visible = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(700, 154)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox3.TabIndex = 12
+        Me.TextBox3.Visible = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(869, 53)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox4.TabIndex = 13
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(869, 103)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox5.TabIndex = 14
+        Me.TextBox5.Visible = False
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(869, 154)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox6.TabIndex = 15
+        Me.TextBox6.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(796, 205)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(601, 53)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 17)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Provincias"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(583, 106)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 17)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "CodProvincia"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(582, 157)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(92, 17)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "N°Habitantes"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1177, 450)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label44)
+        Me.Controls.Add(Me.Label33)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label33 As Label
+    Friend WithEvents Label44 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged1(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        If ComboBox1.SelectedItem = "Costa" Then
+            ComboBox3.Items.Clear()
+            ComboBox3.Visible = True
+            ComboBox3.Items.Add("Esmeraldas")
+            ComboBox3.Items.Add("Santo Domingo")
+            ComboBox3.Items.Add("Manabi­")
+            ComboBox3.Items.Add("Los Rios")
+            ComboBox3.Items.Add("Guayas ")
+            ComboBox3.Items.Add("Santa Elena ")
+            ComboBox3.Items.Add("El Oro")
+
+        ElseIf ComboBox1.SelectedItem = "Sierra" Then
+            ComboBox2.Items.Clear()
+            ComboBox2.Visible = True
+
+            ComboBox2.Items.Add("Carchi")
+            ComboBox2.Items.Add("Imbabura")
+            ComboBox2.Items.Add("Pichincha")
+            ComboBox2.Items.Add("Loja")
+            ComboBox2.Items.Add("Cotopaxi")
+            ComboBox2.Items.Add("Tungurahua")
+            ComboBox2.Items.Add("Bolivar")
+        ElseIf ComboBox1.SelectedItem = "Oriente" Then
+
+            ComboBox3.Items.Clear()
+            ComboBox3.Visible = True
+
+            ComboBox3.Items.Add("Morona Santiago")
+            ComboBox3.Items.Add("Napo")
+            ComboBox3.Items.Add("Orellana")
+            ComboBox3.Items.Add("Pastaza")
+            ComboBox3.Items.Add("Sucumbios ")
+            ComboBox3.Items.Add("Zamora Chinchipe ")
+
+        ElseIf ComboBox1.SelectedItem = "Seleccionar:" Then
+            ComboBox2.Items.Clear()
+            ComboBox3.Items.Clear()
+
+            ComboBox2.Visible = False
+            ComboBox3.Visible = False
+
+            Label3.Text = ""
+            Label4.Text = ""
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            TextBox5.Text = ""
+            TextBox6.Text = ""
+
+
+
+
+        End If
+    End Sub
+    Private Sub ComboBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ComboBox1.KeyUp
+
+
+
+
+        If ComboBox1.SelectedItem = "Costa" Then
+            ComboBox2.Items.Clear()
+            ComboBox2.Visible = True
+            ComboBox2.Items.Add("Esmeraldas")
+            ComboBox2.Items.Add("Santo Domingo")
+            ComboBox2.Items.Add("Manabi­")
+            ComboBox2.Items.Add("Los Rios")
+            ComboBox2.Items.Add("Guayas ")
+            ComboBox2.Items.Add("Santa Elena ")
+            ComboBox2.Items.Add("El Oro")
+
+        ElseIf ComboBox1.SelectedItem = "Oriente" Then
+
+            ComboBox3.Items.Clear()
+            ComboBox3.Visible = True
+
+            ComboBox3.Items.Add("Morona Santiago")
+            ComboBox3.Items.Add("Napo")
+            ComboBox3.Items.Add("Orellana")
+            ComboBox3.Items.Add("Pastaza")
+            ComboBox3.Items.Add("Sucumbios ")
+            ComboBox3.Items.Add("Zamora Chinchipe ")
+
+        ElseIf ComboBox1.SelectedItem = "Seleccionar:" Then
+            ComboBox2.Items.Clear()
+            ComboBox3.Items.Clear()
+
+            ComboBox2.Visible = False
+            ComboBox3.Visible = False
+
+            Label3.Text = ""
+            Label4.Text = ""
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            TextBox5.Text = ""
+            TextBox6.Text = ""
+
+
+
+
+        End If
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        Button1.Visible = True
+
+
+        TextBox4.Text = Convert.ToString(ComboBox3.SelectedItem)
+        TextBox4.Visible = True
+        TextBox5.Visible = True
+        TextBox6.Visible = True
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        TextBox1.Text = Convert.ToString(ComboBox2.SelectedItem)
+        TextBox1.Visible = True
+        TextBox2.Visible = True
+        TextBox3.Visible = True
+
+
+
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim numero1 As Decimal
+        Dim numero2 As Decimal
+
+        numero1 = CDec(TextBox3.Text)
+        numero2 = CDec(TextBox6.Text)
+        If numero1 > numero2 Then
+
+            Label3.Text = TextBox1.Text
+            Label4.Text = TextBox4.Text
+
+        ElseIf numero1 < numero2 Then
+            Label4.Text = TextBox1.Text
+            Label3.Text = TextBox4.Text
+
+
+        End If
+
+
+    End Sub
+
+End Class
